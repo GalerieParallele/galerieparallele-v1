e1455e015e8b9bb1a593d80757022b5208c08c5e
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-08-18 15:56:52 +0200

    Mise en forme du composant Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+
+import styles from '../styles/components/Button.module.css';
+
 export default function Button({text}) {
     return (
-        <button>{text}</button>
+        <button
+            className={styles.button}
+        >
+            {text}
+        </button>
     );
 }
