b1036e092813a3fae2befc0540d12c1879bf22b5
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-08-19 08:19:45 +0200

    Création d'un composant permettant d'indiquer à l'utilisateur qu'il est connecté et lui proposer la déconnexion

---
+++ src/components/authentication/AlreadyLoginComponent.jsx
@@ -0,0 +1,39 @@
+import React, {useCallback, useState} from "react";
+
+import Button from "@/components/Button";
+
+import {useAuth} from "@/hooks/useAuth";
+
+export default function AlreadyLoginComponent() {
+
+    const {user, signOut} = useAuth();
+
+    const [loading, setLoading] = useState(false);
+
+
+    const handleSubmit = useCallback(async (e) => {
+
+        e.preventDefault();
+
+        setLoading(true);
+
+        await signOut();
+
+        setLoading(false);
+
+    }, [])
+
+    return (
+        <>
+            <h2>Vous êtes connecté</h2>
+            <p>Vous pouvez maintenant accéder à votre espace personnel</p>
+            <br/>
+            <p>E-mail: {user.email}</p>
+            <Button
+                onClick={handleSubmit}
+                text={"Déconnexion"}
+                isLoading={loading}
+            />
+        </>
+    );
+}
