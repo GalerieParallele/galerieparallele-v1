0586e537be37484ccafe03cc37bbe32253f080c9
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-08-21 18:29:57 +0200

    Modification des imports de styles

--- src/components/Amo.jsx
+++ src/components/Amo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import styles from '@/styles/components/Amo.module.css'
+import styles from '../styles/components/Amo.module.css'
 
-export default function Amo(){
-    return(
+export default function Amo() {
+    return (
         <div className={styles.amo}>
             <h4>Arts member only</h4>
         </div>
--- src/components/items/IconInput.jsx
+++ src/components/items/IconInput.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import '../../app/globals.css'
-import styles from '@/styles/components/IconInput.module.css';
+import styles from '../../styles/components/IconInput.module.css';
 
 function IconInput({IconComponent, label, type, required, ...inputProps}) {
     return (
