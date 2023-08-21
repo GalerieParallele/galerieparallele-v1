6ff3606fde9a2b6c48bc9d784f7f577557181a38
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-08-21 11:17:38 +0200

    Optimisation de la route /me + fonctionnelle

--- src/app/api/users/me/route.js
+++ src/app/api/users/me/route.js
@@ -1,5 +1,4 @@
 import {NextResponse} from "next/server";
-
 import {PrismaClient} from "@prisma/client";
 
 import jwt from "jsonwebtoken";
@@ -9,31 +8,54 @@ const prisma = new PrismaClient();
 
 const MESSAGES = {
     NO_TOKEN: "Aucun token trouvé dans le cookie.",
-    INVALID_TOKEN: "Token invalide."
+    INVALID_TOKEN: "Token invalide.",
+    COOKIE_GET_ERROR: "Erreur lors de la récupération du cookie.",
+    USER_NOT_FOUND: "Utilisateur non trouvé."
+};
+
+const HTTP_STATUS = {
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    OK: 200
 };
 
 export async function POST(req, res) {
 
-    const tokenFromCookie = cookies().get("token").value;
+    let tokenFromCookie;
+
+    try {
+        tokenFromCookie = cookies().get("token").value;
+    } catch (error) {
+        return NextResponse.json({message: MESSAGES.COOKIE_GET_ERROR}, {status: HTTP_STATUS.BAD_REQUEST});
+    }
 
     if (!tokenFromCookie) {
-        return NextResponse.json({message: MESSAGES.NO_TOKEN}, {status: 401});
+        return NextResponse.json({message: MESSAGES.NO_TOKEN}, {status: HTTP_STATUS.UNAUTHORIZED});
+    }
+
+    let decoded;
+    try {
+        decoded = jwt.verify(tokenFromCookie, process.env.JWT);
+    } catch (err) {
+        return NextResponse.json({message: MESSAGES.INVALID_TOKEN}, {status: HTTP_STATUS.UNAUTHORIZED});
     }
 
     try {
-
-        const decoded = jwt.verify(tokenFromCookie, process.env.JWT);
         const userId = decoded.user.id;
-
         const user = await prisma.user.findUnique({
             where: {
                 id: userId
             }
         });
 
-        return NextResponse.json({id: user.id, email: user.email}, {status: 200})
+        if (!user) {
+            return NextResponse.json({message: MESSAGES.USER_NOT_FOUND}, {status: HTTP_STATUS.BAD_REQUEST});
+        }
 
+        return NextResponse.json({id: user.id, email: user.email}, {status: HTTP_STATUS.OK});
     } catch (err) {
-        return NextResponse.json({message: MESSAGES.INVALID_TOKEN}, {status: 400});
+        return NextResponse.json({message: MESSAGES.INVALID_TOKEN}, {status: HTTP_STATUS.BAD_REQUEST});
+    } finally {
+        await prisma.$disconnect();
     }
 }
