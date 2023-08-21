63ad99de7bb7b7593c7a65d604b7cbe59a090965
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-08-21 12:23:42 +0200

    Création d'une page d'accueil fictive

---
+++ src/pages/.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import {useAuth} from "@/hooks/useAuth";
+import {useCallback} from "react";
+import Button from "@/components/Button";
+
+export default function () {
+
+    const {user, isLoading, signOut} = useAuth()
+
+    const handleSignOut = useCallback(() => {
+        signOut()
+    }, [signOut])
+
+    return (
+        <>
+            <h3>Accueil</h3>
+            <Link href={"/login"}>S'authentifier</Link>
+
+            {user && (
+                <>
+                    <p>Vous êtes connecté en tant que {user.email}</p>
+                    <Button onClick={handleSignOut} text={"Se déconnecter"}/>
+                </>
+            )}
+        </>
+    )
+}
