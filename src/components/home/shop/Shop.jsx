a08db0f9ae1af494295434f1d0ae01e74e8d3dc6
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-08-29 03:14:14 +0200

    Section articles (shop) : OK

--- src/components/home/shop/Shop.jsx
+++ src/components/home/shop/Shop.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 import styles from "@/styles/components/home/Shop.module.css"
+import ShopItem from "@/components/home/shop/ShopItem";
+import Button from "@/components/items/Button";
 
 export default function Shop() {
     return (
         <div className={styles.shop}>
-
+            <div className={styles.list}>
+                <ShopItem
+                    articleName={"Oeuvre test"}
+                    imgSrc={"article1.png"}
+                />
+                <ShopItem
+                    articleName={"Oeuvre test"}
+                    imgSrc={"article1.png"}
+                />
+                <ShopItem
+                    articleName={"Oeuvre test"}
+                    imgSrc={"article1.png"}
+                />
+            </div>
+            <div>
+                <Button
+                    text={"VOIR PLUS"}
+                />
+            </div>
         </div>
     )
 }
--- src/components/home/shop/ShopItem.jsx
+++ src/components/home/shop/ShopItem.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 
-import styles from "@/styles/components/home/ShopItem.module.css"
+import Image from "next/image";
+
 import Button from "@/components/items/Button";
 
-export default function ShopItem() {
+import styles from "@/styles/components/home/ShopItem.module.css";
+
+export default function ShopItem({imgSrc, articleName}) {
     return (
         <div className={styles.shopItem}>
-            <div>
+            <div className={styles.imgContainer}>
+                <Image
+                    src={`/assets/img/shop/${imgSrc}`}
+                    alt={`Photo du shop de l'article ${articleName}`}
+                    fill
+                />
+            </div>
+            <div className={styles.informations}>
                 <h5>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
                     impression.</h5>
                 <Button
                     text={"BOUTON"}
                 />
+            </div>
+            <div className={styles.bottom}>
+                <h5>{articleName}</h5>
             </div>
         </div>
     )
