9a2deb9493fc93dd3dfb1da39bae94bd00efb713
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-10-12 14:46:16 +0200

    Création d'une fausse page profil artiste

---
+++ src/pages/artistes/profil/artisteTest/index.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/items/navbar/Navbar";
+
+export default function artisteTest() {
+    return (
+        <main>
+            <Navbar/>
+            <section>
+
+            </section>
+        </main>
+    )
+}
