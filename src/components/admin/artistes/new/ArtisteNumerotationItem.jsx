c94914f212a3b88712713df11bd4e9eeaae6f66f
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-10-26 12:21:14 +0200

    Oeuvre admin artiste design

---
+++ src/components/admin/artistes/new/ArtisteNumerotationItem.jsx
@@ -0,0 +1,54 @@
+import {useState} from "react";
+
+import styles from './ArtisteNumerotationItem.module.scss';
+
+export default function ArtisteNumerotationItem() {
+
+    const [checked, setChecked] = useState(false);
+    const [inputValue, setInputValue] = useState(2);
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.head}>
+                <div>
+                    <label form="oeuvreUnique">
+                        Oeuvre unique
+                    </label>
+                </div>
+                <div>
+                    <input
+                        type="checkbox"
+                        id="oeuvreUnique"
+                        name="oeuvreUnique"
+                        checked={checked}
+                        onChange={() => {
+                            if (checked) {
+                                setInputValue(2);
+                            }
+                            setChecked(!checked);
+                        }}
+                    />
+                </div>
+            </div>
+            {
+                checked ? null : (
+                    <div className={styles.inputNum}>
+                        <p>{`Oeuvre numérotée et limitée à ${inputValue ? inputValue : 0} exemplaires`}</p>
+                        <input
+                            type="number"
+                            min={2}
+                            value={inputValue}
+                            onChange={(e) => {
+                                if (e.target.value == 1) {
+                                    setChecked(true);
+                                } else {
+                                    setInputValue(e.target.value);
+                                }
+                            }}
+                        />
+                    </div>
+                )
+            }
+        </div>
+    )
+}
--- src/pages/admin/artistes/new/index.js
+++ src/pages/admin/artistes/new/index.js
@@ -4,29 +4,83 @@ import {
     AiFillFacebook,
     AiFillInstagram,
     AiFillLinkedin,
-    AiOutlineArrowLeft,
+    AiOutlineArrowLeft, AiOutlineColumnHeight, AiOutlineColumnWidth,
     AiOutlineFieldNumber
 } from "react-icons/ai";
 import ROUTES from "@/constants/ROUTES";
-import React from "react";
+import React, {useState} from "react";
 import {useRouter} from "next/router";
+
+import makeAnimated from 'react-select/animated';
 
 import adminStyles from "@/pages/admin/articles/AdminArticles.module.css";
 import styles from './Index.module.scss';
 import IconInput from "@/components/items/iconinput/IconInput";
 import {MdEmail, MdPassword} from "react-icons/md";
 import {RxAvatar} from "react-icons/rx";
-import {BsFillFileEarmarkPersonFill, BsTelephoneFill} from "react-icons/bs";
+import {BsFillFileEarmarkPersonFill, BsHammer, BsTelephoneFill} from "react-icons/bs";
 import ArtisteNewSectionItem from "@/components/admin/artistes/new/ArtisteNewSectionItem";
-import {IoHome} from "react-icons/io5";
-import {PiTextIndentBold} from "react-icons/pi";
+import {IoHome, IoTextOutline} from "react-icons/io5";
+import {PiListNumbers, PiStool, PiTextIndentBold, PiUserRectangleBold} from "react-icons/pi";
 import Editor from "@/components/items/Editor";
 import {GrTextAlignCenter} from "react-icons/gr";
 import {FaEarthAfrica} from "react-icons/fa6";
+import Select from 'react-select';
+import {BiGroup, BiLaugh, BiSolidGroup} from "react-icons/bi";
+import {FiHash} from "react-icons/fi";
+import {IoIosResize} from "react-icons/io";
+import ArtisteNumerotationItem from "@/components/admin/artistes/new/ArtisteNumerotationItem";
+import {FaHandHoldingHeart, FaSignature} from "react-icons/fa";
 
 export default function ArtisteAdminNew() {
 
     const router = useRouter();
+
+    const animatedComponents = makeAnimated();
+
+    const [formData, setFormData] = useState({
+        email: '',
+        oeuvreArtist: [],
+        oeuvreUnknownArtist: [],
+        oeuvreType: [],
+        tags: [],
+    });
+
+    const handleChangeArtist = (item) => {
+        setFormData({
+            ...formData,
+            oeuvreArtist: item
+        });
+    };
+
+    const handleChangeUnknownArtist = (item) => {
+        setFormData({
+            ...formData,
+            oeuvreUnknownArtist: item
+        });
+    };
+
+    const handleChangeOeurveType = (item) => {
+        setFormData({
+            ...formData,
+            oeuvreType: item
+        });
+    };
+
+    const handleChangeTags = (item) => {
+        setFormData({
+            ...formData,
+            tags: item
+        });
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+        console.log(formData);
+    };
 
     return (
         <Admin>
@@ -44,13 +98,16 @@ export default function ArtisteAdminNew() {
                     </div>
                     <div className={styles.sectionList}>
                         <ArtisteNewSectionItem
-                            sectionName={"Informations utilisateur"}
+                            sectionName={"Compte utilisateur"}
                         >
                             <IconInput
                                 label={"E-mail"}
                                 type={"email"}
                                 IconComponent={MdEmail}
                                 placeholder={"Ex: example@mail.com"}
+                                onChange={handleChange}
+                                name={"email"}
+                                value={formData.email}
                             />
                             <IconInput
                                 label={"Mot de passe"}
@@ -101,7 +158,74 @@ export default function ArtisteAdminNew() {
                             />
                         </ArtisteNewSectionItem>
                         <ArtisteNewSectionItem
+                            sectionName={"Informations juridiques"}
+                        >
+                            <p>Prochainement...</p>
+                        </ArtisteNewSectionItem>
+                        <ArtisteNewSectionItem
+                            sectionName={"Relations contractuelles"}
+                        >
+                            <p>Prochainement...</p>
+                        </ArtisteNewSectionItem>
+                        <ArtisteNewSectionItem
                             sectionName={"Informations artiste"}
+                        >
+                            <IconInput
+                                label={"Numéro de SIRET (non visible sur le site)"}
+                                type={"text"}
+                                IconComponent={AiOutlineFieldNumber}
+                                placeholder={"Ex: 12345678912345"}
+                            />
+                            <IconInput
+                                label={"Numéro de TVA (non visible sur le site)"}
+                                type={"text"}
+                                IconComponent={AiOutlineFieldNumber}
+                                placeholder={"Ex: 12345678912345"}
+                            />
+                            <IconInput
+                                label={"Nom d'artiste"}
+                                type={"text"}
+                                IconComponent={BsFillFileEarmarkPersonFill}
+                                placeholder={"Ex: M4TRIX"}
+                            />
+                            <IconInput
+                                label={"Facebook"}
+                                type={"text"}
+                                IconComponent={AiFillFacebook}
+                                placeholder={"Ex: https://www.facebook.com/..."}
+                            />
+                            <IconInput
+                                label={"Instagram"}
+                                type={"text"}
+                                IconComponent={AiFillInstagram}
+                                placeholder={"Ex: https://www.instagram.com/..."}
+                            />
+                            <IconInput
+                                label={"LinkedIn"}
+                                type={"text"}
+                                IconComponent={AiFillLinkedin}
+                                placeholder={"Ex: https://www.linkedin.com/..."}
+                            />
+                            <IconInput
+                                label={"Site web"}
+                                type={"text"}
+                                IconComponent={FaEarthAfrica}
+                                placeholder={"Ex: https://www.m4trix.com"}
+                            />
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                    <span>
+                                        <GrTextAlignCenter/>
+                                    </span>
+                                    <div>
+                                        <p>Biographie</p>
+                                    </div>
+                                </div>
+                                <Editor/>
+                            </div>
+                        </ArtisteNewSectionItem>
+                        <ArtisteNewSectionItem
+                            sectionName={"Illustrations"}
                         >
                             <IconInput
                                 label={"Numéro de SIRET (non visible sur le site)"}
@@ -161,6 +285,215 @@ export default function ArtisteAdminNew() {
                             sectionName={"Save The Date"}
                         >
                             <p>Prochainement...</p>
+                        </ArtisteNewSectionItem>
+                        <ArtisteNewSectionItem
+                            sectionName={"Oeuvres"}
+                        >
+                            <IconInput
+                                label={"Nom de l'oeuvre"}
+                                type={"text"}
+                                IconComponent={IoTextOutline}
+                                placeholder={"Ex: Sur le chemin de la vie"}
+                            />
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                        <span>
+                                            <BiSolidGroup/>
+                                        </span>
+                                    <div>
+                                        <p>Artistes connu</p>
+                                    </div>
+                                </div>
+                                <Select
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    defaultValue={[]}
+                                    isMulti
+                                    options={[
+                                        {value: '1', label: 'Richard Orlinksi'},
+                                        {value: '2', label: 'Jean-Michel Basquiat'},
+                                        {value: '3', label: 'Andy Warhol'},
+                                        {value: '4', label: 'Keith Haring'},
+                                        {value: '5', label: 'Banksy'},
+                                        {value: '6', label: 'JR'},
+                                        {value: '7', label: 'Invader'},
+                                        {value: '8', label: 'Kaws'},
+                                        {value: '9', label: 'Shepard Fairey'},
+                                        {value: '10', label: 'Vhils'},
+                                    ]}
+                                    onChange={handleChangeArtist}
+                                    value={formData.oeuvreArtist}
+                                />
+                            </div>
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                        <span>
+                                            <BiGroup/>
+                                        </span>
+                                    <div>
+                                        <p>Artistes non référencé</p>
+                                    </div>
+                                </div>
+                                <Select
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    defaultValue={[]}
+                                    isMulti
+                                    options={[
+                                        {value: '1', label: 'Mathieu'},
+                                        {value: '2', label: 'Jean'},
+                                        {value: '3', label: 'Pierre'},
+                                        {value: '4', label: 'Paul'},
+                                        {value: '5', label: 'Jacques'},
+                                        {value: '6', label: 'Jean-Pierre'},
+                                        {value: '7', label: 'Jean-Jacques'},
+                                        {value: '8', label: 'Jean-Paul'},
+                                        {value: '9', label: 'Pierre-Paul'},
+                                        {value: '10', label: 'Pierre-Jacques'},
+                                    ]}
+                                    onChange={handleChangeUnknownArtist}
+                                    value={formData.oeuvreUnknownArtist}
+                                />
+                            </div>
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                        <span>
+                                            <BsHammer/>
+                                        </span>
+                                    <div>
+                                        <p>Type de l&lsquo;oeuvre</p>
+                                    </div>
+                                </div>
+                                <Select
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    defaultValue={[]}
+                                    isMulti
+                                    options={[
+                                        {value: '1', label: 'Sculpture'},
+                                        {value: '2', label: 'Peinture'},
+                                        {value: '3', label: 'Photographie'},
+                                        {value: '4', label: 'Dessin'},
+                                        {value: '5', label: 'Graffiti'},
+                                        {value: '6', label: 'Street Art'},
+                                        {value: '7', label: 'Art numérique'},
+                                        {value: '8', label: 'Art vidéo'},
+                                        {value: '9', label: 'Art sonore'},
+                                        {value: '10', label: 'Art performance'},
+                                    ]}
+                                    onChange={handleChangeOeurveType}
+                                    value={formData.oeuvreType}
+                                />
+                            </div>
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                        <span>
+                                            <FiHash/>
+                                        </span>
+                                    <div>
+                                        <p>Tags</p>
+                                    </div>
+                                </div>
+                                <Select
+                                    closeMenuOnSelect={false}
+                                    components={animatedComponents}
+                                    defaultValue={[]}
+                                    isMulti
+                                    options={[
+                                        {value: '1', label: 'Sculpture'},
+                                        {value: '2', label: 'Peinture'},
+                                        {value: '3', label: 'Photographie'},
+                                        {value: '4', label: 'Dessin'},
+                                        {value: '5', label: 'Graffiti'},
+                                        {value: '6', label: 'Street Art'},
+                                        {value: '7', label: 'Art numérique'},
+                                        {value: '8', label: 'Art vidéo'},
+                                        {value: '9', label: 'Art sonore'},
+                                        {value: '10', label: 'Art performance'},
+                                    ]}
+                                    onChange={handleChangeTags}
+                                    value={formData.tags}
+                                />
+                            </div>
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                    <span>
+                                        <GrTextAlignCenter/>
+                                    </span>
+                                    <div>
+                                        <p>Description de l&lsquo;oeuvre</p>
+                                    </div>
+                                </div>
+                                <Editor/>
+                            </div>
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                    <span>
+                                        <BiLaugh/>
+                                    </span>
+                                    <div>
+                                        <p>Anecdote</p>
+                                    </div>
+                                </div>
+                                <Editor/>
+                            </div>
+                            <IconInput
+                                label={"Hauteur (en cm)"}
+                                type={"number"}
+                                IconComponent={AiOutlineColumnHeight}
+                                placeholder={"Ex: 120"}
+                                required
+                            />
+                            <IconInput
+                                label={"Longueur (en cm)"}
+                                type={"number"}
+                                IconComponent={AiOutlineColumnWidth}
+                                placeholder={"Ex: 80"}
+                                required
+                            />
+                            <IconInput
+                                label={"Largeur (en cm)"}
+                                type={"number"}
+                                IconComponent={IoIosResize}
+                                placeholder={"Ex: 30"}
+                            />
+                            <div className={styles.specialSection}>
+                                <div className={styles.specialSectionHead}>
+                                    <span>
+                                        <PiListNumbers/>
+                                    </span>
+                                    <div>
+                                        <p>Numérotation</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <ArtisteNumerotationItem/>
+                                </div>
+                            </div>
+                            <IconInput
+                                label={"Support et matériau(x) utilisé(s)"}
+                                type={"text"}
+                                IconComponent={PiStool}
+                                placeholder={"Ex: Papier photo, Toile..."}
+                            />
+                            <IconInput
+                                label={"Technique(s) utilisée(s)"}
+                                type={"text"}
+                                IconComponent={FaHandHoldingHeart}
+                                placeholder={"Ex: Peinture à l'huile, Acrylique..."}
+                            />
+                            <IconInput
+                                label={"Encadrement"}
+                                type={"text"}
+                                IconComponent={PiUserRectangleBold}
+                                placeholder={"Ex: Caisse américaine, Cadre..."}
+                            />
+                            <IconInput
+                                label={"Signature"}
+                                type={"text"}
+                                IconComponent={FaSignature}
+                                placeholder={"Ex: Signé en bas à droite"}
+                            />
                         </ArtisteNewSectionItem>
                     </div>
                 </div>
