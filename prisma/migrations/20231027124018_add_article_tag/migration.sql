b4de74d42f96150eb02e7bc7ee559224915a219c
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-10-27 14:41:59 +0200

    Ajout de la possiblité d'ajouter des tags à un article

---
+++ prisma/migrations/20231027124018_add_article_tag/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `ArticleTag` (
+    `articleId` INTEGER NOT NULL,
+    `tagId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`articleId`, `tagId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ArticleTag` ADD CONSTRAINT `ArticleTag_articleId_fkey` FOREIGN KEY (`articleId`) REFERENCES `Article`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ArticleTag` ADD CONSTRAINT `ArticleTag_tagId_fkey` FOREIGN KEY (`tagId`) REFERENCES `Tag`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
