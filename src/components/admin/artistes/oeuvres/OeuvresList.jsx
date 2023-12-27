5cf53af5d6f1b1285e4b85a42367af611f7473c7
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2023-12-27 10:39:10 +0100

    Intégration des oeuvres dans le composant + création de la feuille de style correspondante

--- src/components/admin/artistes/oeuvres/OeuvresList.jsx
+++ src/components/admin/artistes/oeuvres/OeuvresList.jsx
@@ -1,11 +1,11 @@
+import styles from './OeuvresList.module.scss';
+import OeuvreListItem from "@/components/admin/artistes/oeuvres/OeuvreListItem";
+
 export default function OeuvresList({oeuvres}) {
     return (
         <div className={styles.main}>
             {oeuvres.map((oeuvre, index) => (
-                <div className={styles.oeuvre} key={index}>
-                    <h2>{oeuvre.title}</h2>
-                    <p>{oeuvre.description}</p>
-                </div>
+                <OeuvreListItem oeuvre={oeuvre} key={index}/>
             ))}
         </div>
     )
