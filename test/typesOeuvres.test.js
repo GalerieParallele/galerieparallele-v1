62ca045904b1f5136dd61c7d2964f9b6e9e08ea1
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-03 11:45:34 +0100

    Suppression des tests inutiles

--- test/typesOeuvres.test.js
+++
@@ -1,8 +0,0 @@
-var axios = require('axios');
-var expect = require('chai').expect;
-
-describe('GET - /api/typesoeuvres', function () {
-    it('should return a list of types d\'oeuvre or a not found message', async function () {
-
-    });
-});
