151b2e4a0796cc9e40a171683e02f01c9822b29c
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-04 17:33:37 +0100

    Ajout de la possibilité d'afficher un bouton de retour

--- src/components/dashboard/items/DashboardNavbar.jsx
+++ src/components/dashboard/items/DashboardNavbar.jsx
@@ -7,14 +7,29 @@ import {FaHome} from "react-icons/fa";
 import Link from "next/link";
 import ROUTES from "../../../constants/ROUTES";
 import {useRouter} from "next/router";
+import {AiOutlineArrowLeft} from "react-icons/ai";
 
-export default function DashboardNavbar() {
+export default function DashboardNavbar({returnURL}) {
 
     const router = useRouter();
 
     return (
         <div className={styles.main}>
-            <div/>
+            {
+                returnURL ?
+                    (
+                        <div>
+                            <Button
+                                isWhite
+                                text={<AiOutlineArrowLeft/>}
+                                onClick={() => router.push(returnURL)}
+                            />
+                        </div>
+                    ) :
+                    (
+                        <div/>
+                    )
+            }
             <div className={styles.center}>
                 <Link href={ROUTES.ADMIN.HOME}>
                     <div className={styles.imgContainer}>
