574788c4daccf3a8e96a1cc2cd965539bfe63909
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-05 10:27:47 +0100

    Ajout de la possibilité de montrer/cacher un mot de passe

--- src/components/items/iconinput/IconInput.jsx
+++ src/components/items/iconinput/IconInput.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-
 import '../../../app/globals.css'
 import styles from './IconInput.module.css';
 import Swal from "sweetalert2";
+import {FaEye, FaEyeSlash} from "react-icons/fa";
 
 function IconInput({IconComponent, label, type, required, ...inputProps}) {
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleOpenModalInformationRequired = async () => {
         await Swal.fire(
@@ -15,6 +15,49 @@ function IconInput({IconComponent, label, type, required, ...inputProps}) {
             'info'
         )
     }
+
+    // Fonction pour gérer le rendu en fonction du type
+    const renderInput = () => {
+        switch(type) {
+            case 'textarea':
+                return (
+                    <textarea
+                        {...inputProps}
+                        className={styles.input}
+                        required={required}
+                    />
+                );
+            case 'password':
+                return (
+                    <div className={styles.passwordInput}>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            {...inputProps}
+                            className={styles.input}
+                            required={required}
+                        />
+                        <div className={styles.passwordVisibility}>
+                            <button
+                                type={'button'}
+                                onClick={() => setShowPassword(!showPassword)}
+                                className={styles.showPassword}>
+                                {showPassword ? <FaEyeSlash/> : <FaEye/>}
+                            </button>
+                        </div>
+                    </div>
+                );
+            default:
+                return (
+                    <input
+                        type={type}
+                        {...inputProps}
+                        className={styles.input}
+                        required={required}
+                    />
+                );
+        }
+    }
+
     return (
         <div>
             <div className={styles.top}>
@@ -29,22 +72,7 @@ function IconInput({IconComponent, label, type, required, ...inputProps}) {
                     </span>
                 </p>
             </div>
-            {
-                type === 'textarea' ? (
-                    <textarea
-                        {...inputProps}
-                        className={styles.input}
-                        required={required}
-                    />
-                ) : (
-                    <input
-                        type={type}
-                        {...inputProps}
-                        className={styles.input}
-                        required={required}
-                    />
-                )
-            }
+            {renderInput()}
         </div>
     );
 }
