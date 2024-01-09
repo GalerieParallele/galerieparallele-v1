8df985944097bb1f102f4741862f5d602ae8b919
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-09 10:53:35 +0100

    Ajout de la base de la page d'édition/visualisation des oeuvres d'un artiste

--- src/pages/dashboard/artistes/edit/[id]/oeuvres/index.jsx
+++ src/pages/dashboard/artistes/edit/[id]/oeuvres/index.jsx
@@ -1,14 +1,22 @@
-import styles from './Index.module.scss';
-import DashboardNavbar from "@/components/dashboard/items/DashboardNavbar";
 import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
+
+import ROUTES from "@/constants/ROUTES";
+
+import DashboardNavbar from "@/components/dashboard/items/DashboardNavbar";
 import PageLoader from "@/components/items/PageLoader";
 import Error from "@/components/error/Error";
-import ROUTES from "@/constants/ROUTES";
+import Select from "react-select";
+import Button from "@/components/items/button/Button";
+
+import {useEffect, useState} from "react";
+import {IoIosRefresh} from "react-icons/io";
+
+import styles from './Index.module.scss';
 
 export default function DashboardArtisteEditOeuvres() {
 
     const router = useRouter();
+
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(true);
 
@@ -39,8 +47,37 @@ export default function DashboardArtisteEditOeuvres() {
     return (
         <div className={styles.main}>
             <DashboardNavbar
-                returnURL={ROUTES.ADMIN.ARTISTES.EDIT.HOME(artisteId)}
+                returnURL={ROUTES.ADMIN.HOME}
             />
+            <div className={styles.searchSpace}>
+                <div className={styles.searchBar}>
+                    <Select
+                        placeholder={"Rechercher une oeuvre"}
+                        closeMenuOnSelect={true}
+                        defaultValue={[]}
+                        isMulti={false}
+                        isClearable
+                        // options={selectOptions}
+                        // onChange={setCurrentArtistSearch}
+                        // value={currentArtistSearch}
+                        // isLoading={artistLoading}
+                        // isDisabled={artistLoading}
+                    />
+                </div>
+                <Button
+                    text={"Créer une oeuvre"}
+                    onClick={() => router.push(ROUTES.ADMIN.ARTISTES.NEW)}
+                />
+                <Button
+                    text={<IoIosRefresh/>}
+                    onClick={() => {
+                        // TODO : Ajouter un bouton de rechargement des artistes
+                    }}
+                />
+            </div>
+            <div className={styles.content}>
+                <p>test</p>
+            </div>
         </div>
-    )
+    );
 }
