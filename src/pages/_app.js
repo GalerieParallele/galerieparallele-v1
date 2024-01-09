c91fbbfa274d552e78f11d8a7106d9ad7c09e6e0
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-09 11:51:34 +0100

    Ajout d'un titre général du site ainsi qu'une description

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import {AuthProvider} from "@/hooks/useAuth";
 import NextNProgress from 'nextjs-progressbar';
 import "@/app/globals.css"
+import Head from "next/head";
 
 function MyApp({Component, pageProps}) {
     return (
         <AuthProvider>
+            <Head>
+                <title>GalerieParallèle</title>
+                <description>Bienvenue sur le site officiel de la GalerieParallèle. Retrouvez toutes nos oeuvres,
+                    artistes, et bien plus encore.
+                </description>
+            </Head>
             <NextNProgress options={{easing: 'ease', speed: 500, showSpinner: false}}/>
             <Component {...pageProps} />
         </AuthProvider>
