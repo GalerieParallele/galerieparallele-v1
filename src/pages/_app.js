650443b5eddb1da6994b4f00a489ab9e5a4be976
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-09 12:14:35 +0100

    Début de la partie portrait chinois

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,9 +8,6 @@ function MyApp({Component, pageProps}) {
         <AuthProvider>
             <Head>
                 <title>GalerieParallèle</title>
-                <description>Bienvenue sur le site officiel de la GalerieParallèle. Retrouvez toutes nos oeuvres,
-                    artistes, et bien plus encore.
-                </description>
             </Head>
             <NextNProgress options={{easing: 'ease', speed: 500, showSpinner: false}}/>
             <Component {...pageProps} />
---
+++ src/pages/dashboard/artistes/edit/[id]/portrait-chinois/index.jsx
@@ -0,0 +1,133 @@
+import React, {useEffect, useReducer, useState} from "react";
+
+import {useRouter} from "next/router";
+
+import ROUTES from "@/constants/ROUTES";
+
+import Switch from 'react-switch';
+
+import DashboardNavbar from "@/components/dashboard/items/DashboardNavbar";
+import DashboardSectionItem from "@/components/dashboard/items/sections/DashboardSectionItem";
+import IconInput from "@/components/items/iconinput/IconInput";
+import PageLoader from "@/components/items/PageLoader";
+import Error from "@/components/error/Error";
+
+import {FaQuestion} from "react-icons/fa";
+import {RiQuestionAnswerLine} from "react-icons/ri";
+
+import styles from './Index.module.scss';
+
+const initialState = {
+    pc: {
+        question: undefined,
+        response: undefined,
+        private: false
+    }
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'UPDATE_FORM':
+            const [field, nestedField] = action.payload.field.split('.');
+            return {
+                ...state,
+                [field]: {
+                    ...state[field],
+                    [nestedField]: action.payload.value,
+                },
+            };
+        default:
+            throw new Error();
+    }
+}
+
+export default function DashboardArtisteEditPortraitChinoisIndex() {
+
+    const router = useRouter();
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    const [artisteId, setArtisteId] = useState(null);
+
+    useEffect(() => {
+        if (router.query.id && /^\d+$/.test(router.query.id)) {
+            setArtisteId(router.query.id);
+            setError(false);
+        } else {
+            setError(true);
+        }
+        setLoading(false);
+    }, [router, router.query.id]);
+
+    if (loading) {
+        return <PageLoader/>;
+    }
+
+    if (error) {
+        return <Error
+            code={404}
+            title={"Artiste introuvable"}
+            message={"L'artiste avec l'identifiant \"" + router.query.id + "\" n'existe pas"}
+        />;
+    }
+
+    return (
+        <div className={styles.main}>
+            <DashboardNavbar
+                returnURL={ROUTES.ADMIN.ARTISTES.EDIT.HOME(artisteId)}
+            />
+            <div className={styles.content}>
+                <DashboardSectionItem
+                    sectionName={"Création d'une question"}
+                >
+                    <IconInput
+                        label={"Question"}
+                        type={"text"}
+                        IconComponent={FaQuestion}
+                        placeholder={"Ex: Si j'étais un animal, je serais"}
+                        onChange={(e) => {
+                            // TODO : Modifier le state correspondant
+                        }}
+                        name={"pc.question"}
+                        value={state.pc.question}
+                        disabled={loading}
+                        required
+                    />
+                    <IconInput
+                        label={"Réponse"}
+                        type={"text"}
+                        IconComponent={RiQuestionAnswerLine}
+                        placeholder={"Ex: Un chat"}
+                        onChange={(e) => {
+                            // TODO : Modifier le state correspondant
+                        }}
+                        name={"pc.response"}
+                        value={state.pc.response}
+                        disabled={loading}
+                        required
+                    />
+                    <label>Privé</label>
+                    <Switch
+                        checked={state.pc.private}
+                        onChange={(e) => {
+                            dispatch({
+                                type: 'UPDATE_FORM',
+                                payload: {
+                                    field: 'pc.private',
+                                    value: e
+                                }
+                            })
+                        }}/>
+                    <div>
+                        <button>
+                            Ajouter
+                        </button>
+                    </div>
+                </DashboardSectionItem>
+            </div>
+        </div>
+    )
+}
