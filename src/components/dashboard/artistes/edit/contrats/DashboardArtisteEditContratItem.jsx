be98a21a86bae4001b3c8b36a5c83f57a5912f19
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-10 15:45:42 +0100

    Ajout des boutons sur les dossiers et fichiers dans la relation artiste permettant de supprimer/éditer

--- src/components/dashboard/artistes/edit/contrats/DashboardArtisteEditContratItem.jsx
+++ src/components/dashboard/artistes/edit/contrats/DashboardArtisteEditContratItem.jsx
@@ -2,6 +2,8 @@ import {FcFolder} from "react-icons/fc";
 
 import styles from './DashboardArtisteEditContratItem.module.scss';
 import {FaRegFileLines} from "react-icons/fa6";
+import {MdEdit} from "react-icons/md";
+import {FaRegTrashAlt} from "react-icons/fa";
 
 export default function DashboardArtisteEditContratItem({folder, name}) {
 
@@ -9,11 +11,23 @@ export default function DashboardArtisteEditContratItem({folder, name}) {
         <div className={styles.main}>
             <div>
                 {
-                    folder ? <FcFolder/> : <FaRegFileLines />
+                    folder ? <FcFolder/> : <FaRegFileLines/>
                 }
             </div>
             <div>
                 <p>{name}</p>
+            </div>
+            <div className={styles.actions}>
+                <button
+                className={styles.edit}
+                >
+                    <MdEdit/>
+                </button>
+                <button
+                className={styles.delete}
+                >
+                    <FaRegTrashAlt/>
+                </button>
             </div>
         </div>
     )
