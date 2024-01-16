bd32c6c73167094777911090eb2296b22b76be5f
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-01-16 15:33:59 +0100

    Modification des cartes oeuvres dans l'éditon d'un artiste

--- src/components/dashboard/artistes/edit/oeuvres/DashboardArtisteEditOeuvreCard.jsx
+++ src/components/dashboard/artistes/edit/oeuvres/DashboardArtisteEditOeuvreCard.jsx
@@ -1,42 +1,48 @@
 import Image from "next/image";
 
-import styles from './DashboardArtisteEditOeuvreCard.module.scss';
-import {MdDelete, MdEdit} from "react-icons/md";
-import {FaEye} from "react-icons/fa";
-
 export default function DashboardArtisteEditOeuvreCard({oeuvre}) {
 
     return (
         <div className={styles.main}>
-            <Image
-                src={"https://picsum.photos/200/300"}
-                alt={"Oeuvre"}
-                width={200}
-                height={300}
-            />
-            <div className={styles.oeuvreActions}>
-                <button
-                className={styles.show}
-                >
-                    <FaEye/>
-                </button>
-                <button
-                className={styles.edit}
-                >
-                    <MdEdit/>
-                </button>
-                <button
-                className={styles.delete}
-                >
-                    <MdDelete/>
-                </button>
+            <div className={styles.content}>
+                <div className={styles.top}>
+                    <Image
+                        src={"https://picsum.photos/200/300"}
+                        alt={"Oeuvre"}
+                        width={200}
+                        height={300}
+                    />
+                    <div className={styles.oeuvreActions}>
+                        <button
+                            className={styles.show}
+                        >
+                            <FaEye/>
+                        </button>
+                        <button
+                            className={styles.edit}
+                        >
+                            <MdEdit/>
+                        </button>
+                        <button
+                            className={styles.delete}
+                        >
+                            <MdDelete/>
+                        </button>
+                    </div>
+                </div>
+                <div className={styles.bottom}>
+                    <div className={styles.oeuvreName}>
+                        <div>
+                            <h4>{oeuvre.name}</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
-           <div className={styles.oeuvreName}>
-               <div>
-                   <h4>{oeuvre.name}</h4>
-               </div>
-           </div>
         </div>
-    )
+)
 
 }
+import styles from './DashboardArtisteEditOeuvreCard.module.scss';
+import {MdDelete, MdEdit} from "react-icons/md";
+
+import {FaEye} from "react-icons/fa";
