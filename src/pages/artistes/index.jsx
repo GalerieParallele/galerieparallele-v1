796c32fa3c8e839c0d08caa01d8528ed4a091dba
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-03-19 22:35:40 +0100

    UPDATE : Mise en forme de la page listant les artistes + changement police

--- src/pages/artistes/index.jsx
+++ src/pages/artistes/index.jsx
@@ -3,10 +3,8 @@ import {useRouter} from "next/router";
 import styles from './Index.module.scss';
 import Navbar from "@/components/items/navbar/Navbar";
 import {useArtists} from "@/hooks/useArtists";
-import LittleSpinner from "@/components/items/LittleSpinner";
-import Image from "next/image";
 import {useEffect} from "react";
-import ROUTES from "@/constants/ROUTES";
+import Image from "next/image";
 
 export default function ArtistesHomeIndex() {
 
@@ -18,40 +16,49 @@ export default function ArtistesHomeIndex() {
         console.log(artists);
     }, [artists]);
 
-    return (
-        <div className={styles.main}>
-            <Navbar/>
-            <div style={{
-                padding: "50px",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center"
-            }}>
-                {
-                    loading ? <LittleSpinner/> : (
-                        <div className={styles.artists}>
-                            {artists.map((artist, index) => (
-                                <div
-                                    className={styles.artist}
-                                    key={index}
-                                    style={{
-                                        backgroundColor: "var(--white)",
-                                        boxShadow: "var(--shadow)",
-                                        padding: "20px",
-                                    }}
-                                    onClick={() => router.push(ROUTES.ARTISTES.PROFIL(artist.id))}
-                                >
-                                    <Image src={artist.user.avatarURL || "/assets/img/avatar.png"} width={200} height={200} alt={"Photo test"}/>
-                                    <div className={styles.artistName}>
-                                        {artist.pseudo ? artist.pseudo : artist.user.lastname.toUpperCase() + " " + artist.user.firstname}
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    )
-                }
+    return (<div className={styles.main}>
+        <Navbar/>
+        <div className={styles.bigHead}>
+            <h2>Nos artistes</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
+        </div>
+        <div className={styles.atTheTop}>
+            <h4>Artistes à la une</h4>
+            <div className={styles.atTheTopContainer}>
+                <div className={styles.left}>
+                    <div className={styles.imgContainer}>
+                        <Image
+                            src={'/assets/img/oeuvres/oeuvre2.jpg'}
+                            alt={"Photo oeuvre à la une numéro 2"}
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.imgContainer}>
+                        <Image
+                            src={'/assets/img/oeuvres/oeuvre1.jpg'}
+                            alt={"Photo oeuvre à la une numéro 1"}
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+
+                    <div className={styles.imgContainer}>
+                        <Image
+                            src={'/assets/img/oeuvres/oeuvre3.jpg'}
+                            alt={"Photo oeuvre à la une numéro 3"}
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+
+                </div>
             </div>
         </div>
-    )
+    </div>)
 }
