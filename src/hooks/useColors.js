2129ab10f3d39a315681997f2a8b0d3ab8f2216d
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-03-27 18:32:28 +0100

    ADD : Création du hook de gestion des couleurs

---
+++ src/hooks/useColors.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import {fetchColors} from "@/services/couleurs/couleursServices";
+
+export const useColors = () => {
+
+    const [colors, setColors] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+        setLoading(true);
+
+        fetchColors()
+            .then((response) => {
+                setColors(response.colors)
+            })
+            .catch((error) => {
+                setError(error.message)
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+
+    }, []);
+
+    return {colors, loading, error};
+
+}
