088a01709b368bf7e9c641fee253731fca4d7a0d
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-03-27 09:34:59 +0100

    ADD : Ajout du router pour permettre la navigation vers la page listant toutes les oeuvres

--- src/components/home/shop/Shop.jsx
+++ src/components/home/shop/Shop.jsx
@@ -3,8 +3,13 @@ import React from "react";
 import styles from "@/components/home/shop/Shop.module.css"
 import ShopItem from "@/components/home/shop/ShopItem";
 import Button from "@/components/ui/button/Button";
+import {useRouter} from "next/router";
+import ROUTES from "@/constants/ROUTES";
 
 export default function Shop() {
+
+    const router = useRouter();
+
     return (
         <div className={styles.shop}>
             <div className={styles.list}>
@@ -24,6 +29,7 @@ export default function Shop() {
             <div className={styles.buttonSpace}>
                 <Button
                     text={"VOIR PLUS"}
+                    onClick={() => router.push(ROUTES.OEUVRES.HOME)}
                 />
             </div>
         </div>
