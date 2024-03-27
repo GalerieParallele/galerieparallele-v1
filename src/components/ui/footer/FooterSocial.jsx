edc3bf3c967eaa4a96ddb0cd21cfc91e406636c4
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-03-27 09:32:44 +0100

    FIX : Correction erreur d'orthographe

--- src/components/ui/footer/FooterSocial.jsx
+++ src/components/ui/footer/FooterSocial.jsx
@@ -1,5 +1,5 @@
 import styles from "./FooterSocial.module.scss";
 
 export default function FooterSocial(){
-    return <p className={styles.paragraph}>Partagez vos expériences artistiques et vos plus belles décorations avec le #GalerieParrallele</p>
+    return <p className={styles.paragraph}>Partagez vos expériences artistiques et vos plus belles décorations avec le #GalerieParallele</p>
 }
