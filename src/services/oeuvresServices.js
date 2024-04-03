1b6dc3891431e85b5e3bdb1e8f68b59217de507a
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-03 23:17:39 +0200

    ADD : Création d'un service pour les oeuvres

---
+++ src/services/oeuvresServices.js
@@ -0,0 +1,40 @@
+import ROUTES from "@/constants/ROUTES";
+
+export const fetchOeuvreById = async (id) => {
+
+    try {
+
+        const response = await fetch(ROUTES.API.OEUVRES.GETBYID, {
+            method: 'POST',
+            body: JSON.stringify({id}),
+        });
+
+        if (!response.ok) {
+
+            const errorDetails = await response.text();
+
+            throw {
+                message: `Erreur HTTP: statut ${response.status}, détails: ${errorDetails}`,
+                code: response.status
+            };
+
+        }
+
+        let data = await response.json();
+
+        if (!data) {
+            throw {message: "Format de réponse invalide", code: 'InvalidFormat'};
+        }
+
+        return {success: true, oeuvre: data};
+
+    }catch (error) {
+
+        if (process.env.NODE_ENV === 'development') {
+            console.error("[Service] Erreur lors de la récupération des oeuvres:", error);
+        }
+
+        return {success: false, error: {message: error.message, code: error.code}};
+
+    }
+};
