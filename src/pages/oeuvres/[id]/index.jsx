a4faf3e010b63911e1a338a25c84f5587e935497
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-03 23:16:58 +0200

    UPDATE : Dynamisme de la page oeuvre

--- src/pages/oeuvres/[id]/index.jsx
+++ src/pages/oeuvres/[id]/index.jsx
@@ -26,6 +26,10 @@ import {AiOutlineFieldNumber} from "react-icons/ai";
 import MultiCarousel from "@/components/ui/carousel/MultiCarousel";
 import {IoSend} from "react-icons/io5";
 import LittleSpinner from "@/components/ui/LittleSpinner";
+import {useOeuvres} from "@/hooks/useOeuvres";
+import PageLoader from "@/components/ui/PageLoader";
+import Error from "@/components/error/Error";
+import ROUTES from "@/constants/ROUTES";
 
 export default function OeuvreHomePage() {
 
@@ -45,10 +49,22 @@ export default function OeuvreHomePage() {
     const [messageValue, setMessageValue] = useState('');
     const [convLoading, setConvLoading] = useState(false);
 
+    const {oeuvre, loading: oeuvreLoading, error: oeuvreError, getOeuvreById} = useOeuvres();
+
+    /**
+     * Ajouter un nouveau message à la conversation
+     * @param type
+     * @param content
+     */
     const handleAddNewMessage = (type, content) => {
         setMessage(prevMessages => [...prevMessages, [type, content]]);
     }
 
+    /**
+     * Envoyer un message à l'API
+     * @param userMessage
+     * @returns {Promise<void>}
+     */
     const sendMessageToAPI = async (userMessage) => {
         setConvLoading(true);
         try {
@@ -75,6 +91,10 @@ export default function OeuvreHomePage() {
         }
     };
 
+    /**
+     * Gérer l'envoi d'un message
+     * @returns {Promise<void>}
+     */
     const handleSendMessage = async () => {
         if (!messageValue.trim()) return; // Ne rien faire si le message est vide ou ne contient que des espaces
 
@@ -82,6 +102,14 @@ export default function OeuvreHomePage() {
         await sendMessageToAPI(messageValue);
         setMessageValue(''); // Réinitialiser le champ de texte après l'envoi
     };
+
+    let sendHelloMessage = false;
+
+    useEffect(() => {
+        if (sendHelloMessage) return;
+        handleAddNewMessage(MESSAGE_TYPE.RESPONSE, "Bonjour, je suis un assistant virtuel, je suis là pour répondre à vos questions. N'hésitez pas à me poser des questions sur cette oeuvre.");
+        sendHelloMessage = true;
+    }, []);
 
     useEffect(() => {
 
@@ -98,13 +126,44 @@ export default function OeuvreHomePage() {
 
     }, [router, router.query.id]);
 
-    let sendHelloMessage = false;
-
     useEffect(() => {
-        if (sendHelloMessage) return;
-        handleAddNewMessage(MESSAGE_TYPE.RESPONSE, "Bonjour, je suis un assistant virtuel, je suis là pour répondre à vos questions. N'hésitez pas à me poser des questions sur cette oeuvre.");
-        sendHelloMessage = true;
-    }, []);
+        if (oeuvreId) {
+            const fetchOeuvreById = getOeuvreById(oeuvreId)
+            if (!fetchOeuvreById) {
+                setError(true);
+            }
+        }
+    }, [oeuvreId]);
+
+    if (loading || oeuvreLoading) {
+        return <PageLoader/>;
+    }
+
+    if (error) {
+        return <Error
+            code={400}
+            title={"Oops..."}
+            message={"Une erreur est survenue, il semble que l'identifiant de l'oeuvre soit incorrect."}
+        />;
+    }
+
+    if (oeuvreError) {
+        console.log(oeuvreError);
+        switch (oeuvreError.code) {
+            case 404:
+                return <Error
+                    code={404}
+                    title={"Oeuvre introuvable"}
+                    message={"L'oeuvre que vous recherchez n'existe pas ou a été supprimée."}
+                />;
+            default:
+                return <Error
+                    code={400}
+                    title={"Oops..."}
+                    message={"Une erreur est survenue lors de la requête de récupération de l'oeuvre."}
+                />;
+        }
+    }
 
     return (
         <div className={styles.main}>
@@ -112,26 +171,37 @@ export default function OeuvreHomePage() {
             <div className={styles.content}>
                 <div className={styles.headSection}>
                     <div className={styles.carouselContainer}>
-                        <Carousel images={[
-                            '/assets/img/no-img.jpg',
-                            '/assets/img/harcourt2.jpg',
-                            '/assets/img/harcourt1.jpg'
-                        ]}/>
+                        <Carousel
+                            images={oeuvre && oeuvre.images ? oeuvre.images.map(image => image.url) : ['/assets/img/no-img.jpg']}/>
                     </div>
                     <div className={styles.right}>
                         <div className={styles.info}>
                             <div className={styles.head}>
                                 <div className={styles.oeuvreName}>
-                                    <h2>La joconde</h2>
+                                    <h2>{oeuvre && oeuvre.name}</h2>
                                     <span>
                                     <FaRegHeart className={styles.emptyHeart}/>
                                     <FaHeart
                                         style={styles.fillHeart}/>
                                 </span>
                                 </div>
-                                <Link href={"#"}>
-                                    <h5>Leonard de Vinci</h5>
-                                </Link>
+                                <span style={{
+                                    display: 'flex',
+                                    gap: 3,
+                                }}>
+                                    {
+                                        oeuvre && oeuvre.artists.map((artist, index) => {
+                                            return (
+                                                <Link href={ROUTES.ARTISTES.PROFIL(artist.id)} key={index}>
+                                                    <h5 key={index}>
+                                                        {artist.pseudo ? artist.pseudo : artist.user.lastname.toUpperCase() + " " + artist.user.firstname}
+                                                        {index < oeuvre.artists.length - 1 ? ', ' : ''}
+                                                    </h5>
+                                                </Link>
+                                            )
+                                        })
+                                    }
+                                </span>
                             </div>
                             <div style={{
                                 display: 'flex',
@@ -140,7 +210,7 @@ export default function OeuvreHomePage() {
                                 width: '100%',
                             }}>
                                 <OeuvreTarif
-                                    prix={100}
+                                    prix={oeuvre && oeuvre.prix}
                                 />
                             </div>
                             <div className={styles.shareContainer}>
@@ -155,22 +225,19 @@ export default function OeuvreHomePage() {
                                 </div>
                             </div>
                         </div>
-                        <div className={styles.tagsSection}>
-                            <span>#Leonard de Vinci</span>
-                            <span>#Peinture</span>
-                            <span>#Portrait</span>
-                            <span>#Art</span>
-                            <span>#Leonard de Vinci</span>
-                            <span>#Portrait</span>
-                            <span>#Art</span>
-                            <span>#Leonard de Vinci</span>
-                            <span>#Portrait</span>
-                            <span>#Art</span>
-                            <span>#Leonard de Vinci</span>
-                            <span>#Portrait</span>
-                            <span>#Art</span>
-                            <span>#Leonard de Vinci</span>
-                        </div>
+                        {
+                            oeuvre && oeuvre.tags.length > 0 && (
+                                <div className={styles.tagsSection}>
+                                    {
+                                        oeuvre.tags.map((tag, index) => {
+                                            return (
+                                                <span key={index}>#{tag}</span>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
                 <div className={styles.featureAndIASection}>
@@ -178,44 +245,46 @@ export default function OeuvreHomePage() {
                         <OeuvreFeatureItem
                             Icon={FaRulerHorizontal}
                             title={"Dimensions (cm)"}
-                            content={"153 x 77 x 5"}
-                            description={"Hauteur x Largeur x Profondeur"}
+                            content={oeuvre && (
+                                oeuvre.hauteur + ' x ' + oeuvre.longueur + (oeuvre.profondeur ? ' x ' + oeuvre.profondeur : '')
+                            )}
+                            description={"Hauteur x Largeur" + (oeuvre && oeuvre.profondeur ? ' x Profondeur' : '')}
                         />
                         <OeuvreFeatureItem
                             Icon={TbFileOrientation}
                             title={"Orientation"}
-                            content={"Portrait"}
+                            content={oeuvre && oeuvre.orientation ? oeuvre.orientation : "Non renseignée"}
                         />
                         <OeuvreFeatureItem
                             Icon={FaChair}
                             title={"Support"}
-                            content={"Toile"}
+                            content={oeuvre && oeuvre.support ? oeuvre.support : "Non renseigné"}
                         />
                         <OeuvreFeatureItem
                             Icon={FaPaintBrush}
                             title={"Technique"}
-                            content={"Peinture à l'huile"}
+                            content={oeuvre && oeuvre.technique ? oeuvre.technique : "Non renseignée"}
                         />
                         <OeuvreFeatureItem
                             Icon={MdOutlineFilterFrames}
                             title={"Encadrement"}
-                            content={"Cadre en bois"}
+                            content={oeuvre && oeuvre.encadrement ? oeuvre.encadrement : "Non renseigné"}
                         />
                         <span className={styles.separator}/>
                         <OeuvreFeatureItem
                             Icon={FaSignature}
                             title={"Signature"}
-                            content={"En bas à droite"}
+                            content={oeuvre && oeuvre.signature ? oeuvre.signature : "Non renseignée"}
                         />
                         <OeuvreFeatureItem
                             Icon={AiOutlineFieldNumber}
                             title={"Numérotation"}
-                            content={"Oeuvre unique"}
+                            content={oeuvre && oeuvre.numerotation ? (oeuvre.numerotation == 1 ? "Œuvre unique" : oeuvre.numerotation) : "Non renseignée"}
                         />
                         <OeuvreFeatureItem
                             Icon={AiOutlineFieldNumber}
                             title={"Limitation"}
-                            content={"Oeuvre unique"}
+                            content={oeuvre && oeuvre.limitation ? oeuvre.limitation == 1 ? "Œuvre unique" : oeuvre.limitation : "Non renseignée"}
                         />
                     </div>
                     <div className={styles.moreInfoContainer}>
@@ -307,33 +376,40 @@ export default function OeuvreHomePage() {
                     <MultiCarousel/>
                 </div>
                 <div className={styles.artistSection}>
-                    <div className={styles.artistContainer}>
-                        <div className={styles.left}>
-                            <div className={styles.imgContainer}>
-                                <Image src={'/assets/img/avatar.png'} alt={"Photo de l'artiste"} width={500}
-                                       height={500}/>
-                                <div className={styles.flag}>
-                                    <Image src={"/assets/img/drapeau_france.png"} alt={"Drapeau de la France"}
-                                           width={50} height={50}/>
+                    {
+                        oeuvre && oeuvre.artists.length > 1 && oeuvre.artists.map((artist, index) => {
+                            return <div className={styles.artistContainer} key={index}>
+                                <div className={styles.left}>
+                                    <div className={styles.imgContainer}>
+                                        <Image
+                                            src={artist && artist.user && artist.user.avatarURL ? artist.user.avatarURL : '/assets/img/avatar.png'}
+                                            alt={"Photo de l'artiste"} width={500}
+                                            height={500}/>
+                                        <div className={styles.flag}>
+                                            <Image src={"/assets/img/drapeau_france.png"} alt={"Drapeau de la France"}
+                                                   width={50} height={50}/>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h3>{artist.pseudo ? artist.pseudo : artist.user.lastname.toUpperCase() + " " + artist.user.firstname}</h3>
+                                    </div>
+                                </div>
+                                <div>
+                                    {
+                                        artist.bio && artist.bio.length > 200 ? artist.bio.substr(0, 200) + '...' : artist.bio
+                                    }
+                                    <Link
+                                        href={ROUTES.ARTISTES.PROFIL(artist.id)}
+                                        className={styles.showMore}
+                                    >
+                                        <button>
+                                            Voir plus
+                                        </button>
+                                    </Link>
                                 </div>
                             </div>
-                            <div>
-                                <h3>Leonard de Vinci</h3>
-                            </div>
-                        </div>
-                        <div>
-                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                                invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-                                accusam
-                                et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                                Lorem ipsum dolor sit amet...</p>
-                            <div className={styles.showMore}>
-                                <button>
-                                    Voir plus
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                        })
+                    }
                 </div>
                 <div className={styles.autreOeuvreArtiste}>
                     <h2>Découvrez d&apos;autres oeuvres du même artiste</h2>
