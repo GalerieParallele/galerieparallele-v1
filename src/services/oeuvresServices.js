ce2e1b9e3218d1f24ffacaa933968399c4bcce93
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-04 12:20:52 +0200

    ADD : Ajout dans le service concernant le oeuvre la méthode permettant de récupérer la liste des oeuvres

--- src/services/oeuvresServices.js
+++ src/services/oeuvresServices.js
@@ -1,5 +1,46 @@
 import ROUTES from "@/constants/ROUTES";
 
+export const fetchOeuvres = async () => {
+
+    try {
+
+        const response = await fetch(ROUTES.API.OEUVRES.HOME, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+
+            const errorDetails = await response.text();
+
+            throw {
+                message: `Erreur HTTP: statut ${response.status}, détails: ${errorDetails}`,
+                code: response.status
+            };
+
+        }
+
+        let data = await response.json();
+
+        if (!data || !Array.isArray(data.list)) {
+            throw {message: "Format de réponse invalide", code: 'InvalidFormat'};
+        }
+
+        return {success: true, oeuvres: data.list};
+
+    } catch (error) {
+
+        if (process.env.NODE_ENV === 'development') {
+            console.error("Erreur lors de la récupération des oeuvres:", error);
+        }
+
+        return {success: false, error: {message: error.message, code: error.code}};
+
+    }
+}
+
 export const fetchOeuvreById = async (id) => {
 
     try {
@@ -28,7 +69,7 @@ export const fetchOeuvreById = async (id) => {
 
         return {success: true, oeuvre: data};
 
-    }catch (error) {
+    } catch (error) {
 
         if (process.env.NODE_ENV === 'development') {
             console.error("[Service] Erreur lors de la récupération des oeuvres:", error);
