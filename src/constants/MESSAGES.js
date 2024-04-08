abeab3cebbd8d371613909dc522614d9bc289fef
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-08 17:21:03 +0200

    UPDATE : Modification du message d'erreur lors d'une erreur d'intéraction avec l'API

--- src/constants/MESSAGES.js
+++ src/constants/MESSAGES.js
@@ -2,7 +2,7 @@ const MESSAGES = {
     SUCCESS: {
         SIGN_OUT: "Déconnexion réussie.",
     },
-    API_SERVER_ERROR: "Une erreur serveur est survenue.",
+    API_SERVER_ERROR: "Une erreur serveur est survenue lors de l'intéraction avec l'API.",
 }
 
 export default MESSAGES;
