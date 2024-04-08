fc39e9252dcd46573e17b5903eb6503ce3519471
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-08 19:05:50 +0200

    DELETE : Suppression des fichiers concernant l'ancien panel administrateur

--- src/pages/admin/index.js
+++
@@ -1,9 +0,0 @@
-import Admin from "@/components/admin/Admin";
-import AdminStat from "@/components/admin/home/AdminStat";
-
-export default function Index() {
-
-    return <Admin>
-        <AdminStat/>
-    </Admin>
-}
