65aa1310cd6c3bedac43d6362d8bc68cdff6e381
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-09 17:04:35 +0200

    FIX : Mise à jour du service concernant les artistes

--- src/services/artistesServices.js
+++ src/services/artistesServices.js
@@ -1,80 +1,71 @@
 import ROUTES from "@/constants/ROUTES";
+import {createStandardResponse} from "@/utils/errorUtils";
+import {STATIC_MESSAGES} from "@/constants/STATIC_MESSAGES";
+
+async function handleResponse(response) {
+    if (!response.ok) {
+        const errorDetails = await response.text();
+        throw new Error(`Erreur HTTP: statut ${response.status}, détails: ${errorDetails}`);
+    }
+    return await response.json();
+}
+
+function handleFormatResponse(success, data = null, error = null) {
+    return {
+        success,
+        data,
+        error,
+    };
+}
 
 export const fetchArtists = async () => {
     try {
-
         const response = await fetch(ROUTES.API.ARTISTES.HOME, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            headers: {'Content-Type': 'application/json'},
         });
-
-        if (!response.ok) {
-
-            const errorDetails = await response.text();
-
-            throw {
-                message: `Erreur HTTP: statut ${response.status}, détails: ${errorDetails}`,
-                code: response.status
-            };
-
-        }
-
-        let data = await response.json();
-
-        if (!data || !Array.isArray(data.list)) {
-            throw {message: "Format de réponse invalide", code: 'InvalidFormat'};
-        }
-
-        return {success: true, artists: data.list};
-
+        const data = await handleResponse(response);
+        return createStandardResponse(true, data.list);
     } catch (error) {
-
-        if (process.env.NODE_ENV === 'development') {
-            console.error("Erreur lors de la récupération des artistes:", error);
-        }
-
-        return {success: false, error: {message: error.message, code: error.code}};
+        return createStandardResponse(false, null, {message: error.message});
     }
 };
 
-export const fetchArtistByid = async (id) => {
+export const fetchArtistById = async (id) => {
 
     try {
-
-        const response = await fetch(ROUTES.API.ARTISTES.GETBYID, {
+        const response = await fetch(`${ROUTES.API.ARTISTES.GETBYID}`, {
             method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({id}),
         });
 
-        if (!response.ok) {
-
-            const errorDetails = await response.text();
-
-            throw {
-                message: `Erreur HTTP: statut ${response.status}, détails: ${errorDetails}`,
-                code: response.status
-            };
-
+        switch (response.status) {
+            case 200:
+                return createStandardResponse(true, await response.json());
+            case 404:
+                return createStandardResponse(false, null, {message: "L'identifiant de renseigné ne correspond à aucun artiste.", code: 404});
+            case 403:
+            case 401:
+                return createStandardResponse(false, null, {
+                    message: "Vous n'êtes pas autorisé à accéder à cette ressource.",
+                    code: response.status
+                });
+            default:
+                return createStandardResponse(false, null, {
+                    message: STATIC_MESSAGES.API_SERVER_ERROR,
+                    code: 500
+                });
         }
-
-        let data = await response.json();
-
-        if (!data) {
-            throw {message: "Format de réponse invalide", code: 'InvalidFormat'};
-        }
-
-        return {success: true, artist: data};
 
     } catch (error) {
 
-        console.log(error)
+        return createStandardResponse(false, null, {
+            message: STATIC_MESSAGES.API_SERVER_ERROR,
+            code: 500
+        });
 
-        if (process.env.NODE_ENV === 'development') {
-            console.error("Erreur lors de la récupération des artistes:", error);
-        }
-
-        return {success: false, error: {message: error.message, code: error.code}};
     }
-}
+};
