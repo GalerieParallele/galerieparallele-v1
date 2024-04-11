5aba30fc71d98e4bb4b3cc51817af060300026f6
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-11 16:52:09 +0200

    UPDATE : Suppression du code inutilisé sans la page de création d'un artiste

--- src/pages/dashboard/artistes/new/index.jsx
+++ src/pages/dashboard/artistes/new/index.jsx
@@ -49,18 +49,6 @@ const initialState = {
         linkedin: undefined,
         website: undefined,
     },
-    // legal: {
-    //     societe: undefined,
-    //     adrNumVoie: undefined,
-    //     adrRue: undefined,
-    //     adrVille: undefined,
-    //     adrCodePostal: undefined,
-    //     siret: undefined,
-    //     tva: undefined,
-    //     tauxTva: undefined,
-    //     numMaisonsDesArtistes: undefined,
-    //     numSecuriteSociale: undefined,
-    // }
 };
 
 function reducer(state, action) {
@@ -209,25 +197,6 @@ export default function DashboardArtistesNewIndex() {
                     throw new Error(artistsResponseJSON.message);
 
                 }
-
-                // const artistId = artistsResponseJSON.id;
-                //
-                //
-                // let legalResponse;
-                //
-                // legalResponse = await fetch(ROUTES.API.ARTISTES.LEGAL_INFORMATION, {
-                //     method: 'POST',
-                //     headers: {'Content-Type': 'application/json'},
-                //     body: JSON.stringify({
-                //         ...state.legal,
-                //         artistId
-                //     }),
-                // });
-                //
-                // const legalResponseJSON = await legalResponse.json();
-                //
-                // if (!legalResponse.ok) throw new Error(legalResponseJSON.message);
-
 
                 Toast.fire({icon: 'success', title: 'Artiste créé avec succès'});
 
@@ -508,101 +477,6 @@ export default function DashboardArtistesNewIndex() {
                         />
                     </div>
                 </DashboardSectionItem>
-                {/*<DashboardSectionItem*/}
-                {/*    sectionName={"Informations Juridique"}*/}
-                {/*    description={"Ces informations ne seront pas visibles du grand public"}*/}
-                {/*>*/}
-                {/*    <IconInput*/}
-                {/*        label={"Nom de société"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={BsBuildingsFill}*/}
-                {/*        placeholder={"Ex: DP Gallery"}*/}
-                {/*        name={"legal.societe"}*/}
-                {/*        value={state.legal.societe}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Numéro de voie"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={IoHome}*/}
-                {/*        placeholder={"Ex: 1"}*/}
-                {/*        name={"legal.adrNumVoie"}*/}
-                {/*        value={state.legal.adrNumVoie}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Nom de la voie"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={IoHome}*/}
-                {/*        placeholder={"Ex: Rue de Paris"}*/}
-                {/*        name={"legal.adrRue"}*/}
-                {/*        value={state.legal.adrRue}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Ville"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={IoHome}*/}
-                {/*        placeholder={"Ex: Paris"}*/}
-                {/*        name={"legal.adrVille"}*/}
-                {/*        value={state.legal.adrVille}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Code postal"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={IoHome}*/}
-                {/*        placeholder={"Ex: 75000"}*/}
-                {/*        name={"legal.adrCodePostal"}*/}
-                {/*        value={state.legal.adrCodePostal}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Numéro de SIRET (non visible sur le site)"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={AiOutlineFieldNumber}*/}
-                {/*        placeholder={"Ex: 12345678912345"}*/}
-                {/*        name={"legal.siret"}*/}
-                {/*        value={state.legal.siret}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Numéro de TVA (non visible sur le site)"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={AiOutlineFieldNumber}*/}
-                {/*        placeholder={"Ex: 12345678912345"}*/}
-                {/*        name={"legal.tva"}*/}
-                {/*        value={state.legal.tva}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Taux de TVA applicable (non visible sur le site)"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={FaPercentage}*/}
-                {/*        placeholder={"Ex: 5.5%"}*/}
-                {/*        name={"legal.tauxTva"}*/}
-                {/*        value={state.legal.tauxTva}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Numéro maison des artistes (non visible sur le site)"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={AiOutlineFieldNumber}*/}
-                {/*        placeholder={"Ex: 12345678912345"}*/}
-                {/*        name={"legal.numMaisonsDesArtistes"}*/}
-                {/*        value={state.legal.numMaisonsDesArtistes}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*    <IconInput*/}
-                {/*        label={"Numéro de sécurité sociale (non visible sur le site)"}*/}
-                {/*        type={"text"}*/}
-                {/*        IconComponent={AiOutlineFieldNumber}*/}
-                {/*        placeholder={"Ex: 12345678912345"}*/}
-                {/*        name={"legal.numSecuriteSociale"}*/}
-                {/*        value={state.legal.numSecuriteSociale}*/}
-                {/*        onChange={handleChange}*/}
-                {/*    />*/}
-                {/*</DashboardSectionItem>*/}
                 <div className={styles.topSpace}>
                     <Button
                         text={"Créer l'artiste"}
