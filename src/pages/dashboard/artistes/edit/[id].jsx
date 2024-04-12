bd7af21d9f1bc4038bd2091b6858f0c416d494e8
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-12 16:50:21 +0200

    UPDATE : Récupération de l'id artiste directement dans le router dans la page d'accueil d'édition artiste

--- src/pages/dashboard/artistes/edit/[id].jsx
+++ src/pages/dashboard/artistes/edit/[id].jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useRouter} from "next/router";
 import DashboardNavbar from "@/components/dashboard/items/DashboardNavbar";
 import DashboardArtistesEditTuilesList from "@/components/dashboard/artistes/edit/DashboardArtistesEditTuilesList";
@@ -11,34 +11,18 @@ export default function DashboardArtisteEditIndex() {
 
     const router = useRouter();
 
-    const [artisteId, setArtisteId] = useState(null);
-
     const {
         artist,
         getArtistById,
         loading
     } = useArtistsStore();
 
-    useEffect(() => {
-
-        const routerId = router.query.id;
-
-        if (routerId && /^\d+$/.test(routerId)) {
-
-            setArtisteId(parseInt(routerId));
-
-        }
-
-    }, [router, router.query.id]);
-
     /**
      * Permet de récupérer les informations de l'artiste
      */
     useEffect(() => {
-        if (artisteId) {
-            getArtistById(artisteId);
-        }
-    }, [artisteId, getArtistById])
+        getArtistById(router.query.id);
+    }, [getArtistById, router.query.id])
 
     if (loading) {
         return <PageLoader/>;
