0515fafc53d810096e04002620e7d4706b92a59f
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-15 09:03:59 +0200

    ADD : Ajout des blocs d'édition d'une oeuvre

--- src/pages/dashboard/artistes/edit/[id]/oeuvres/edit/[oeuvreId]/index.jsx
+++ src/pages/dashboard/artistes/edit/[id]/oeuvres/edit/[oeuvreId]/index.jsx
@@ -1,7 +1,14 @@
 import {useRouter} from "next/router";
 import ROUTES from "@/constants/ROUTES";
+
 import DashboardNavbar from "@/components/dashboard/items/DashboardNavbar";
 import DashboardTuiles from "@/components/dashboard/items/DashboardTuiles";
+
+import styles from './Index.module.scss';
+import {IoIosResize, IoMdColorPalette} from "react-icons/io";
+import {FaImage, FaInfoCircle, FaUser} from "react-icons/fa";
+import {LuPlus} from "react-icons/lu";
+import {IoHammer} from "react-icons/io5";
 
 export default function DashboardArtisteOeuvreEdit() {
 
@@ -10,8 +17,42 @@ export default function DashboardArtisteOeuvreEdit() {
     return (
         <div>
             <DashboardNavbar returnURL={ROUTES.ADMIN.ARTISTES.EDIT.OEUVRES(router.query.id)}/>
-            <div>
-
+            <div className={styles.main}>
+                <DashboardTuiles
+                    IconComponent={FaInfoCircle}
+                    name={"Informations générales"}
+                    to={"#"}
+                />
+                <DashboardTuiles
+                    IconComponent={IoHammer}
+                    name={"Informations techniques"}
+                    to={"#"}
+                />
+                <DashboardTuiles
+                    IconComponent={FaImage}
+                    name={"Images"}
+                    to={"#"}
+                />
+                <DashboardTuiles
+                    IconComponent={IoMdColorPalette}
+                    name={"Couleurs"}
+                    to={"#"}
+                />
+                <DashboardTuiles
+                    IconComponent={FaUser}
+                    name={"Artistes"}
+                    to={"#"}
+                />
+                <DashboardTuiles
+                    IconComponent={IoIosResize}
+                    name={"Dimensions"}
+                    to={"#"}
+                />
+                <DashboardTuiles
+                    IconComponent={LuPlus}
+                    name={"Autres"}
+                    to={"#"}
+                />
             </div>
         </div>
     )
