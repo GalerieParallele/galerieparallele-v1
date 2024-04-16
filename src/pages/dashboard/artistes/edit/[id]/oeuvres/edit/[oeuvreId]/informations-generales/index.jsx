4b0acb5108dd1d5fec446c82c26d57c3544ac4a8
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-16 11:35:18 +0200

    UPDATE : Edition des informations générales d'une oeuvre terminé

--- src/pages/dashboard/artistes/edit/[id]/oeuvres/edit/[oeuvreId]/informations-generales/index.jsx
+++ src/pages/dashboard/artistes/edit/[id]/oeuvres/edit/[oeuvreId]/informations-generales/index.jsx
@@ -17,13 +17,15 @@ import sectionStyles from "@/components/dashboard/items/sections/DashboardSectio
 import useUpdateOeuvre from "@/stores/useUpdateOeuvre";
 import {useOeuvres} from "@/hooks/useOeuvres";
 import {Toast} from "@/constants/ToastConfig";
+import Skeleton from "@/components/ui/Skeleton";
 
 export default function HomeOeuvreDashboardEditInfoGen() {
 
     const router = useRouter();
 
-    const artisteId = router.query.id;
-    const oeuvreId = router.query.oeuvreId;
+    const [artisteId, setArtisteId] = useState(null);
+    const [oeuvreId, setOeuvreId] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const {
         formData,
@@ -37,8 +39,6 @@ export default function HomeOeuvreDashboardEditInfoGen() {
         loading: oeuvreLoading,
     } = useOeuvres();
 
-
-    const [loading, setLoading] = useState(true);
 
     const handleSubmit = async () => {
 
@@ -75,6 +75,7 @@ export default function HomeOeuvreDashboardEditInfoGen() {
             });
 
             router.push(ROUTES.ADMIN.ARTISTES.EDIT.OEUVRES.EDIT(artisteId, oeuvreId));
+
         } else {
             Toast.fire({
                 icon: 'error',
@@ -84,81 +85,135 @@ export default function HomeOeuvreDashboardEditInfoGen() {
     }
 
     useEffect(() => {
-
         resetFormData();
-        setLoading(false);
 
-    }, [resetFormData])
+    }, [resetFormData]);
+
+    // if touch ctrl + s submit form
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (e.ctrlKey && e.key === 's') {
+                e.preventDefault();
+                handleSubmit();
+            }
+        }
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    })
 
     useEffect(() => {
-        getOeuvreById(oeuvreId);
-    }, [])
+        if (router.isReady) {
+            setArtisteId(router.query.id);
+            setOeuvreId(router.query.oeuvreId);
+        }
+    }, [router, router.isReady]);
+
+    useEffect(() => {
+        if (oeuvreId)
+            getOeuvreById(oeuvreId)
+                .catch(() => {
+                    Toast.fire({
+                        icon: 'error',
+                        title: "Une erreur est survenue lors de la récupération de l'oeuvre."
+                    });
+                });
+    }, [oeuvreId])
 
     return (
         <div className={styles.main}>
             <DashboardNavbar
-                returnURL={ROUTES.ADMIN.ARTISTES.EDIT.OEUVRES.EDIT(artisteId, oeuvreId)}/>
+                returnURL={ROUTES.ADMIN.ARTISTES.EDIT.OEUVRES.EDIT(artisteId, oeuvreId)}
+            />
             <div className={styles.content}>
-                <div className={styles.topSpace}>
-                    <Button
-                        text={"Enregistrer"}
-                        type={"submit"}
-                        onClick={handleSubmit}
-                        disabled={loading}
-                        isLoading={loading}
-                    />
-                </div>
-                <DashboardSectionItem
-                    sectionName={"Informations générales"}
-                    required
-                >
-                    <IconInput
-                        label={"Nom de l'oeuvre"}
-                        type={"text"}
-                        IconComponent={MdDriveFileRenameOutline}
-                        placeholder={oeuvre && oeuvre.name ? oeuvre.name : "Ex: Mona lisa"}
-                        value={formData.informations_generales.name}
-                        name={"name"}
-                        onChange={(e) => updateFormData("informations_generales.name", e.target.value)}
-                        disabled={loading}
-                    />
-                    <div className={sectionStyles.specialSection}>
-                        <div className={sectionStyles.specialSectionHead}>
+                {
+                    oeuvreLoading || loading || !oeuvre ? (
+                        <>
+                            <div className={styles.topSpace}>
+                                <div style={{
+                                    width: '120px',
+                                    height: '40px',
+                                }}>
+                                    <Skeleton/>
+                                </div>
+                            </div>
+                            <div>
+                                <div
+                                    style={{
+                                        width: '100%',
+                                        height: '70px',
+                                    }}
+                                >
+                                    <Skeleton/>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className={styles.topSpace}>
+                                <Button
+                                    text={"Enregistrer"}
+                                    type={"submit"}
+                                    onClick={handleSubmit}
+                                    disabled={loading}
+                                    isLoading={loading}
+                                />
+                            </div>
+                            <DashboardSectionItem
+                                sectionName={"Informations générales"}
+                            >
+                                <IconInput
+                                    label={"Nom de l'oeuvre"}
+                                    type={"text"}
+                                    IconComponent={MdDriveFileRenameOutline}
+                                    placeholder={oeuvre && oeuvre.name ? oeuvre.name : "Ex: Mona lisa"}
+                                    value={formData.informations_generales.name}
+                                    name={"name"}
+                                    onChange={(e) => updateFormData("informations_generales.name", e.target.value === "" || oeuvre.name ? undefined : e.target.value)}
+                                    disabled={loading}
+                                />
+                                <div className={sectionStyles.specialSection}>
+                                    <div className={sectionStyles.specialSectionHead}>
                                     <span>
                                         <MdOutlineDescription/>
                                     </span>
-                            <p>Description</p>
-                        </div>
-                        <Editor
-                            onEditorChange={(content) => updateFormData("informations_generales.description", content)}
-                            defaultContent={oeuvre && oeuvre.description ? oeuvre.description : ""}
-                        />
-                    </div>
-                    <div className={sectionStyles.specialSection}>
-                        <div className={sectionStyles.specialSectionHead}>
+                                        <p>Description</p>
+                                    </div>
+                                    <Editor
+                                        defaultContent={oeuvre && oeuvre.description ? oeuvre.description : ""}
+                                        onEditorChange={(content) => updateFormData("informations_generales.description", content === oeuvre.description ? undefined : content)}
+                                    />
+                                </div>
+                                <div className={sectionStyles.specialSection}>
+                                    <div className={sectionStyles.specialSectionHead}>
                                     <span>
                                         <FaExclamation/>
                                     </span>
-                            <p>Anecdote</p>
-                        </div>
-                        <Editor
-                            onEditorChange={(content) => updateFormData("informations_generales.anecdote", content)}
-                            defaultContent={oeuvre && oeuvre.anecdote ? oeuvre.anecdote : ""}
-                        />
-                    </div>
-                    <IconInput
-                        label={"Prix"}
-                        step={"0.01"}
-                        type={"number"}
-                        IconComponent={FaEuroSign}
-                        placeholder={oeuvre && oeuvre.prix ? oeuvre.prix : "Ex: 1000"}
-                        // onChange={}
-                        name={"prix"}
-                        value={formData.informations_generales.prix}
-                        disabled={loading}
-                        required
-                    />
-                </DashboardSectionItem>
+                                        <p>Anecdote</p>
+                                    </div>
+                                    <Editor
+                                        onEditorChange={(content) => updateFormData("informations_generales.anecdote", content === oeuvre.anecdote ? undefined : content)}
+                                        defaultContent={oeuvre && oeuvre.anecdote ? oeuvre.anecdote : ""}
+                                    />
+                                </div>
+                                <IconInput
+                                    label={"Prix"}
+                                    step={"0.01"}
+                                    type={"number"}
+                                    IconComponent={FaEuroSign}
+                                    placeholder={oeuvre && oeuvre.prix ? oeuvre.prix : "Ex: 1000"}
+                                    onChange={(e) => updateFormData("informations_generales.prix", e.target.value === "" || parseFloat(e.target.value) === oeuvre.prix ? undefined : parseFloat(e.target.value))}
+                                    name={"prix"}
+                                    value={formData.informations_generales.prix}
+                                    disabled={loading}
+                                />
+                            </DashboardSectionItem>
+                        </>
+                    )
+                }
             </div>
         </div>
     )
