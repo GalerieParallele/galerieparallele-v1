5074d81abb3c167350ba09ccee1d1bb592b89fa6
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-16 14:34:13 +0200

    ADD : Création d'un store pour les oeuvres

---
+++ src/stores/oeuvresStore.js
@@ -0,0 +1,42 @@
+import {create} from "zustand";
+
+import {fetchOeuvreById, fetchOeuvres} from "@/services/oeuvresServices";
+
+const useOeuvresStore = create((set, get) => ({
+    oeuvres: [],
+    oeuvre: null,
+    loading: false,
+    error: null,
+    removeOeuvreById: (id) => {
+        set({oeuvres: get().oeuvres.filter(oeuvre => oeuvre.id !== id)});
+    },
+    reloadOeuvres: async () => {
+        set({loading: true, error: null});
+        try {
+            const response = await fetchOeuvres();
+            if (!response.success) throw new Error(response.error.message);
+            set({oeuvres: response.data});
+        } catch (error) {
+            set({error: error.message});
+        } finally {
+            set({loading: false});
+        }
+    },
+    getOeuvreById: async (id) => {
+        set({loading: true, error: null});
+        try {
+            const response = await fetchOeuvreById(parseInt(id));
+
+            if (!response.success) {
+                throw new Error(response.error);
+            }
+
+            set({oeuvre: response.data});
+
+        } catch (error) {
+            set({error: error});
+        } finally {
+            set({loading: false});
+        }
+    }
+}));
