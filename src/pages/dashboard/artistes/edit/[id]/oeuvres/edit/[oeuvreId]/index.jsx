f5dad34b286a921414f2fc5fd497143befb4075b
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-04-16 14:34:39 +0200

    UPDATE : Section édition d'une oeuvre non terminée = cachées

--- src/pages/dashboard/artistes/edit/[id]/oeuvres/edit/[oeuvreId]/index.jsx
+++ src/pages/dashboard/artistes/edit/[id]/oeuvres/edit/[oeuvreId]/index.jsx
@@ -26,36 +26,36 @@ export default function DashboardArtisteOeuvreEdit() {
                     name={"Informations générales"}
                     to={ROUTES.ADMIN.ARTISTES.EDIT.OEUVRES.INFO_GEN(artisteId, oeuvreId)}
                 />
-                <DashboardTuiles
-                    IconComponent={IoHammer}
-                    name={"Informations techniques"}
-                    to={"#"}
-                />
-                <DashboardTuiles
-                    IconComponent={FaImage}
-                    name={"Images"}
-                    to={"#"}
-                />
-                <DashboardTuiles
-                    IconComponent={IoMdColorPalette}
-                    name={"Couleurs"}
-                    to={"#"}
-                />
-                <DashboardTuiles
-                    IconComponent={FaUser}
-                    name={"Artistes"}
-                    to={"#"}
-                />
-                <DashboardTuiles
-                    IconComponent={IoIosResize}
-                    name={"Dimensions"}
-                    to={"#"}
-                />
-                <DashboardTuiles
-                    IconComponent={LuPlus}
-                    name={"Autres"}
-                    to={"#"}
-                />
+                {/*<DashboardTuiles*/}
+                {/*    IconComponent={IoHammer}*/}
+                {/*    name={"Informations techniques"}*/}
+                {/*    to={"#"}*/}
+                {/*/>*/}
+                {/*<DashboardTuiles*/}
+                {/*    IconComponent={FaImage}*/}
+                {/*    name={"Images"}*/}
+                {/*    to={"#"}*/}
+                {/*/>*/}
+                {/*<DashboardTuiles*/}
+                {/*    IconComponent={IoMdColorPalette}*/}
+                {/*    name={"Couleurs"}*/}
+                {/*    to={"#"}*/}
+                {/*/>*/}
+                {/*<DashboardTuiles*/}
+                {/*    IconComponent={FaUser}*/}
+                {/*    name={"Artistes"}*/}
+                {/*    to={"#"}*/}
+                {/*/>*/}
+                {/*<DashboardTuiles*/}
+                {/*    IconComponent={IoIosResize}*/}
+                {/*    name={"Dimensions"}*/}
+                {/*    to={"#"}*/}
+                {/*/>*/}
+                {/*<DashboardTuiles*/}
+                {/*    IconComponent={LuPlus}*/}
+                {/*    name={"Autres"}*/}
+                {/*    to={"#"}*/}
+                {/*/>*/}
             </div>
         </div>
     )
