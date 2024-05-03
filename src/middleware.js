c83997f9901f6123f2d01003b8cbbf931194e88a
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-05-03 17:10:46 +0200

    ADD : Ajout du middleware permettant de sécuriser l'accès au tableau de bord administrateur

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,47 @@
 import {NextResponse} from "next/server";
+import ROUTES from "@/constants/ROUTES";
 
 export default async function middleware(request) {
 
     const path = request.nextUrl.pathname;
 
+    const user = await checkIfUserIsLogged(request);
+
     if (path.startsWith("/dashboard")) {
-        return NextResponse.next();
+
+        if (!user || !user.roles.includes("ROLE_ADMIN")) {
+            return redirectTo(request, "/auth")
+        }
+
     }
+
+    return NextResponse.next();
+
+}
+
+async function checkIfUserIsLogged(request) {
+    try {
+
+        const response = await fetch(ROUTES.API.AUTH.ME, {
+            method: 'POST',
+            headers: {
+                "Cookie": request.headers.get("cookie") || "",
+            },
+        })
+
+        return await response.json();
+
+    } catch (error) {
+        return false;
+    }
+}
+
+function redirectTo(request, path) {
+
+    const url = request.nextUrl.clone();
+    url.pathname = path;
+    return NextResponse.redirect(url);
+
 }
 
 export const config = {
