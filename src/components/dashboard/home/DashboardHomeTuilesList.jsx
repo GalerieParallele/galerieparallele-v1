b9d8402f49a758c1153d2fb2fb67800896659b71
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2024-05-03 09:56:22 +0200

    ADD : Ajout de la tuile permettant d'accéder à la section "Page d'accueil" sur le tableau de bord admin

--- src/components/dashboard/home/DashboardHomeTuilesList.jsx
+++ src/components/dashboard/home/DashboardHomeTuilesList.jsx
@@ -1,12 +1,17 @@
 import DashboardTuiles from "../items/DashboardTuiles";
 
 import {IoIosMan} from "react-icons/io";
-import {FaPaintBrush} from "react-icons/fa";
+import {FaHome, FaPaintBrush} from "react-icons/fa";
 import ROUTES from "@/constants/ROUTES";
 
 export default function DashboardHomeTuilesList() {
     return (
         <>
+            <DashboardTuiles
+                name={"Page d'accueuil"}
+                IconComponent={FaHome}
+                to={ROUTES.ADMIN.ACCUEIL.ACTUALITY.HOME}
+            />
             <DashboardTuiles
                 name={"Artistes"}
                 IconComponent={IoIosMan}
